b3ca405d97709567a6d0cec51b333860316f41b7
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2024-02-20 02:13:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+function App(){
+  return(
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <img src={logo} className="App-logo" alt="logo" />
+        <p className='raaj'>
+          Hey Raaj aaj kuch naya karte hai naa
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
